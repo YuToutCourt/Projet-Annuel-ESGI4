38a104274f0cce0e5b558e0eb9a0e22b2cd971d8
YùToutCourt <wapzif@gmail.com>
2024-04-24 16:53:53 +0200

    db

--- GARE_SNCTF/app/db-itertools.py
+++ GARE_SNCTF/app/db-itertools.py
@@ -1,7 +1,12 @@
 import sqlite3
 import datetime
 import uuid
-from random import randint
+from random import randint, choice
+import string
+
+
+from itertools import *
+
 
 def connectToDatabase():
     conn = sqlite3.connect('db.sqlite3')
@@ -84,10 +89,39 @@ def random_date():
 date_str_list = [datetime.datetime.strftime(random_date() - datetime.timedelta(days=x), '%Y-%m-%d') for x in range(2000)]
 
 
-insertTrain(connectToDatabase(), str(uuid.uuid4()), 'express', 'voie3', 'uWugon', date_str_list[1], '12:00', 'CUIVRE', 1)
+# insertTrain(connectToDatabase(), str(uuid.uuid4()), 'express', 'voie3', 'uWugon', date_str_list[1], '12:00', 'CUIVRE', 1)
 # print(date_str_list)
 # insertTrain(connectToDatabase(), str(uuid.uuid4()), 'train2', 'voie2', 'wagon2', date_str_list[1], '12:00', 'OR', 1)
 # print(getTrains(connectToDatabase()))
 # deleteTrain(connectToDatabase(), '1')
 
 
+def random_hour_str():
+    return f"{randint(0, 23)}:{randint(0, 59)}"
+
+
+def random_freight():
+    return choice(["OR", "CUIVRE", "ARGENT", "PLATINE", "DIAMANT", "RUBIS", "EMERAUDE", "SAPHIR", "TOPAZE", "AMETHYSTE", "PERLE", "JADE", "OPALE", "ONYX", "AGATE", "MALACHITE", "LAPIS-LAZULI", "TURQUOISE", "CORAIL", "NACRE", "IVOIRE", "EBENE", "ACAJOU", "PALISSANDRE", "CHENE", "ERABLE", "NOYER", "MERISIER", "BAMBOU", "ROSEAU", "OSIER", "JONC", "BAMBIN", "SAULE", "TILLEUL", "PEUPLIER", "CHARME", "HETRE", "CHATAIGNIER"])
+
+
+def random_track():
+    return f"{randint(1, 10)}{choice(string.ascii_uppercase)}"
+
+def random_wagon():
+    return f"{randint(1, 100)}{choice(string.ascii_uppercase)}"
+
+
+with open("./nom_train.txt", "r") as f:
+    names = f.read().split("\n")
+
+    for _ in range(10_000):
+        insertTrain(connectToDatabase(), 
+                    str(uuid.uuid4()), choice(names), 
+                    random_track(), random_wagon(), 
+                    choice(date_str_list), 
+                    random_hour_str(), 
+                    random_freight(), 
+                    randint(0, 1)
+                )
+
+    
