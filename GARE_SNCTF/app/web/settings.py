f2c91ad1f6ddebb724affc976aeb1be0547fe4ed
YùToutCourt <wapzif@gmail.com>
2024-07-18 15:22:51 +0200

    update to debug == false

--- GARE_SNCTF/app/web/settings.py
+++ GARE_SNCTF/app/web/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q=6+vq)103^-7intsl$t8oab&+m0k!(*6d-x1**vc)do5^)=wf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
